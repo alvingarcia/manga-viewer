f1da891b48b63ccca6bef95e635bed132d7bf240
Alvin Garcia <agarcia1337@gmail.com>
2013-09-30 14:04:22 +0800

    Add index page

--- manga_viewer.rb
+++ manga_viewer.rb
@@ -4,5 +4,26 @@ require 'bundler/setup'
 require 'sinatra'
 
 get '/' do
-  'Hello there!'
+  erb :index, locals: {
+                manga: manga_collection
+              }
+end
+
+
+def manga_collection
+  path       = File.join('manga', '**', '*.jpg')
+  collection = {}
+
+  Dir.glob(path).each do |item|
+    title, chapter, img = item.split(/\/|\\/)[1..3]
+
+    return if img.nil?
+
+    collection[title]          ||= {}
+    collection[title][chapter] ||= []
+
+    collection[title][chapter].push(img)
+  end
+
+  collection
 end
