c1fd349c5fcdd2b37ccb8a47eeede72e1559b172
Alvin Garcia <agarcia1337@gmail.com>
2013-10-01 17:34:13 +0800

    Add navigation and design

--- manga_viewer.rb
+++ manga_viewer.rb
@@ -3,21 +3,21 @@ require 'bundler/setup'
 
 require 'sinatra'
 require 'sinatra/static_assets'
-require 'pry'
+
 
 get '/' do
   erb :index, locals: {
-                collection: manga_collection.keys
+                collection: Manga.new.titles
               }
 end
 
 
 get '/:title' do |title|
-  chapters = manga_collection[title].keys
+  manga = Manga.new(title)
 
   erb :chapters, locals: {
                    title:    title,
-                   chapters: chapters
+                   chapters: manga.chapters
                  }
 end
 
@@ -29,32 +29,98 @@ end
 
 
 get '/*/*/*' do |title, chapter, page|
-  image = manga_collection[title][chapter][page.to_i - 1]
+  manga = Manga.new(title, chapter, page)
 
   erb :image, locals: {
-                title:   title,
-                chapter: chapter,
-                page:    page,
-                image:   image
+                title:    title,
+                chapter:  chapter,
+                page:     page,
+                image:    manga.current_image,
+                next_img: manga.next_image_link,
+                prev_img: manga.prev_image_link
               }
 end
 
 
-def manga_collection
-  path       = File.join('public', 'images', '**', '**', '*.jpg')
-  collection = {}
+class Manga
 
-  Dir.glob(path).each do |item|
-    title, chapter, img = item.split(/\/|\\/)[2..4]
+  attr_accessor :host
 
-    next if img.nil?
+  def initialize(title = nil, chapter = nil, page = 1)
+    fetch_collection
 
-    collection[title]          ||= {}
-    collection[title][chapter] ||= []
-
-    item.slice! 'public/'
-    collection[title][chapter].push(item)
+    @title   = title
+    @chapter = chapter
+    @page    = page.to_i - 1
   end
 
-  collection
+
+  def titles
+    @collection.keys
+  end
+
+  def chapters
+    @collection[@title].keys
+  end
+
+  def current_image
+    chapter_images(@chapter)[@page]
+  end
+
+  def next_image_link
+    if @page < (chapter_images.size - 1)
+      return "/#{ @title }/#{ @chapter }/#{ @page + 2 }"
+    end
+
+    chapter_index = chapters.index(@chapter)
+
+    if chapter_index < (chapters.size - 1)
+      return "/#{ @title }/#{ chapters[chapter_index + 1] }/1"
+    end
+
+    "/#{ @title }"
+  end
+
+  def prev_image_link
+    if @page != 0
+      return "/#{ @title }/#{ @chapter }/#{ @page }"
+    end
+
+    chapter_index = chapters.index(@chapter)
+
+    if chapter_index != 0
+      prev_chapter = chapters[chapter_index - 1]
+      prev_page    = chapter_images(prev_chapter).size
+      return "/#{ @title }/#{ prev_chapter }/#{ prev_page }"
+    end
+
+    "/#{ @title }"
+  end
+
+  private
+
+
+  def chapter_images(chapter = @chapter)
+    @collection[@title][chapter]
+  end
+
+  def fetch_collection
+    path       = File.join('public', 'images', '**', '**', '*.jpg')
+    collection = {}
+
+    Dir.glob(path).each do |item|
+      title, chapter, img = item.split(/\/|\\/)[2..4]
+
+      next if img.nil?
+
+      collection[title]          ||= {}
+      collection[title][chapter] ||= []
+
+      item.slice! 'public/'
+      collection[title][chapter].push(item)
+    end
+
+    @collection = collection
+  end
+
 end
